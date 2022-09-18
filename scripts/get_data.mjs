4ec0c8c51e76d46c4f24b6b16508af40d8307898
Espen Klem <espen.klem@gmail.com>
2022-09-18 21:00:14 +0200

    dataset + script for creating it

---
+++ scripts/get_data.mjs
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch'
+import Geohash from 'latlon-geohash';
+import { writeFile } from 'node:fs'
+
+// Get photos
+const response = await fetch('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=4c118b2a6828ec58ece162c08fce298c&format=json&nojsoncallback=1&id=breial&photoset_id=72157600841917142&extras=geo,license,owner_name,tags,views,url_l')
+const data = await response.json()
+
+
+// grab only the photos with geo location data
+function findGeoPhotos (photos) {
+  // set aray that is to be returned
+  const geoPhotos = photos.filter(photo => photo.geo_is_public === 1)
+  return geoPhotos
+}
+
+const documentProcess = function (photos) {
+  // clean object
+  const photosProcessed = photos.map(function(photoObj) {
+    return {
+      id: photoObj.id,
+      title: photoObj.title,
+      licenseid: photoObj.license,
+      license: '© All rights reserved',
+      ownername: photoObj.ownername,
+      tags: photoObj.tags,
+      latitude: photoObj.latitude,
+      longitude: photoObj.longitude,
+      geohash: Geohash.encode(photoObj.latitude, photoObj.longitude),
+      url: photoObj.url_l,
+      height: photoObj.height_l,
+      width: photoObj.width_l
+    }
+  })
+  return photosProcessed
+}
+
+function writeJson (fileName, json) {
+  const string = JSON.stringify(json, null, '  ')
+  writeFile('./test_data.json', string, (err) => {
+    if (err) throw err;
+    console.log('successfully written test_data.json')
+  })
+}
+
+const geoPhotos = findGeoPhotos(data.photoset.photo)
+const geoPhotosProcessed = documentProcess(geoPhotos)
+writeJson ('./geo_dataset.json', geoPhotosProcessed)
