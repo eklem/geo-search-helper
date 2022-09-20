5514224100b2c8d934be2ba86a014c8ea33f2344
Espen Klem <espen.klem@gmail.com>
2022-09-20 09:13:08 +0200

    Added AVA tests for CJS and ESM

--- dist/gsh.cjs.js
+++ dist/gsh.cjs.js
@@ -245,19 +245,20 @@ var latlonGeohash = /*#__PURE__*/Object.freeze({
     'default': Geohash
 });
 
+// Haversine formula
 // Calculating Harviestine distance between two lat/lon-points
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in km
+function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
   return d
 }
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
+function deg2rad (deg) {
+  return deg * (Math.PI / 180)
 }
 
 exports.Geohash = latlonGeohash;
---
+++ test/test.cjs.js
@@ -0,0 +1,30 @@
+const test = require('ava')
+const { getDistanceFromLatLonInKm } = require('../dist/gsh.cjs.js')
+
+test('distance between Oslo and Trondheim in km', (t) => {
+  t.plan(1)
+  const osloTrondheim = 391.1506651059311
+  const osloTrondheimCalculated = getDistanceFromLatLonInKm(59.92163880374441, 10.748807812690696, 63.43533499958433, 10.394401877202329)
+  t.deepEqual(osloTrondheimCalculated, osloTrondheim)
+})
+
+test('distance between Oslo and Drammen in km', (t) => {
+  t.plan(1)
+  const osloDrammen = 35.99041520108006
+  const osloDrammenCalculated = getDistanceFromLatLonInKm(59.92163880374441, 10.748807812690696, 59.74868088039004, 10.204349443896618)
+  t.deepEqual(osloDrammenCalculated, osloDrammen)
+})
+
+test('distance between Oslo and Bergen in km', (t) => {
+  t.plan(1)
+  const osloBergen = 304.75143002827195
+  const osloBergenCalculated = getDistanceFromLatLonInKm(59.92163880374441, 10.748807812690696, 60.405517171897536, 5.325002075776659)
+  t.deepEqual(osloBergenCalculated, osloBergen)
+})
+
+test('distance between Oslo and North Cape in km', (t) => {
+  t.plan(1)
+  const osloNorthcape = 1420.4342433086122
+  const osloNorthcapeCalculated = getDistanceFromLatLonInKm(59.92163880374441, 10.748807812690696, 71.17150234604291, 25.783662073920766)
+  t.deepEqual(osloNorthcapeCalculated, osloNorthcape)
+})
